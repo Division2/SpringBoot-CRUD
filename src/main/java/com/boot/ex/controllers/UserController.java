334aa6b03a688bdc2e8c3e4f60696f7cd16c1ce0
Division2 <freegunz2@naver.com>
2021-12-16 13:35:52 +0900

    throw 수정, Import 삭제

--- src/main/java/com/boot/ex/controllers/UserController.java
+++ src/main/java/com/boot/ex/controllers/UserController.java
@@ -1,12 +1,9 @@
 package com.boot.ex.controllers;
 
-import com.boot.ex.exception.ResourceNotFoundException;
 import com.boot.ex.exception.StatusCode;
 import com.boot.ex.models.data.UserData;
-import com.boot.ex.models.responses.ErrorResponse;
 import com.boot.ex.models.responses.UserResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boot.ex.services.UserService;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,8 +20,10 @@ public class UserController {
 	private final UserService service;
 
 	@GetMapping("/users/{UserID}")
-	public ResponseEntity<?> select(@PathVariable("UserID") String userid) throws ResourceNotFoundException {
+	public ResponseEntity<?> select(@PathVariable("UserID") String userid) throws Exception {
 
-		return ResponseEntity.ok().body(new UserResponse(StatusCode.OK, "회원 정보 조회", service.findByUserID(userid)));
+		UserData user = service.findByUserID(userid);
+
+		return ResponseEntity.ok().body(new UserResponse(StatusCode.OK, "회원 정보 조회", user));
 	}
 }
--- src/main/java/com/boot/ex/services/UserService.java
+++ src/main/java/com/boot/ex/services/UserService.java
@@ -2,14 +2,11 @@ package com.boot.ex.services;
 
 import com.boot.ex.exception.ResourceNotFoundException;
 import com.boot.ex.models.data.UserData;
-import com.boot.ex.models.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import com.boot.ex.repositories.UserRepository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,7 +15,7 @@ public class UserService {
 
 	//회원 ID 조회
 	@Transactional(readOnly = true)
-	public UserData findByUserID(String userid) throws ResourceNotFoundException {
+	public UserData findByUserID(String userid) throws Exception {
 		return repository.findByUserid(userid).orElseThrow(() -> new ResourceNotFoundException("요청한 리소스를 찾을 수 없습니다.")).getData();
 	}
 }
