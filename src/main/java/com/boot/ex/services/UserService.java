665a473ec7a84c9888e753372c89cb73b44bfbd7
Division2 <freegunz2@naver.com>
2021-12-16 18:30:34 +0900

    임시

--- src/main/java/com/boot/ex/services/UserService.java
+++ src/main/java/com/boot/ex/services/UserService.java
@@ -26,6 +26,16 @@ public class UserService {
 		return repository.findByUserid(userid).orElseThrow(() -> new ResourceNotFoundException("요청한 리소스를 찾을 수 없습니다.")).getData();
 	}
 
+	private User save(UserRequest request) throws Exception {
+		User result = repository.save(User.builder()
+				.userid(request.getUserid())
+				.password(passwordEncoder.encode(request.getPassword()))
+				.name(request.getName())
+				.phone(request.getPhone())
+				.build());
+		return result;
+	}
+
 	@Transactional
 	public User insert(UserRequest request) throws Exception {
 
@@ -35,12 +45,19 @@ public class UserService {
 			throw new AleadyExistException("이미 등록된 사용자입니다.");
 		}
 
-		User result = repository.save(User.builder()
-						.userid(request.getUserid())
-						.password(passwordEncoder.encode(request.getPassword()))
-						.name(request.getName())
-						.phone(request.getPhone())
-						.build());
-		return result;
+		return save(request);
+	}
+
+	@Transactional
+	public User update(UserRequest request) throws Exception {
+
+		Optional<User> user = repository.findByUserid(request.getUserid());
+
+		if (user.isPresent()) {
+			return save(request);
+		}
+		else {
+			throw new ResourceNotFoundException("요청한 리소스를 찾을 수 없습니다.");
+		}
 	}
 }
